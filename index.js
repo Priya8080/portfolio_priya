919895356be09ff41c9dc481643b1cacfa7e0e90
Priya Kumari <kumaripriya8084785476@gmail.com>
2026-02-03 17:16:02 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const text = "Web Developer";
-const typed = document.querySelector(".text");
-let index = 0;
 
-function typeLoop() {
-  if (index <= text.length) {
-    typed.textContent = text.slice(0, index);
-    index++;
-    setTimeout(typeLoop, 200); 
-  } else {
-    setTimeout(() => {
-      typed.textContent = "";
-      index = 0;
-      typeLoop();
-    }, 1000); 
-  }
-}
-typeLoop();
